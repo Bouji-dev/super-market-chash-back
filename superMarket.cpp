8b833b3babe0de801d8860dfc413401017b62313
BoujiDev <dev2024loper@gmail.com>
2024-10-14 21:31:21 +0330

    Add project files.

---
+++ superMarket.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int s1, s2, s5, s10;
+int amount;
+
+
+
+void print() {
+	cout << "The papar money (10000) T : " << s10 << " PCS" << endl;
+	cout << "The papar money (5000) T  : " << s5 << " PCS" << endl;
+	cout << "The papar money (2000) T  : " << s2 << " PCS" << endl;
+	cout << "The papar money (1000) T  : " << s1 << " PCS" << endl;
+
+}
+
+int main() {
+	s1, s2, s5, s10 = 0;
+	
+	cout << "How much do you want to give back ? :" << endl;
+	cin >> amount;
+
+	
+	s10 = amount / 10000;
+	amount %= 10000;
+
+	s5 = amount / 5000;
+	amount %= 5000;
+
+	s2 = amount / 2000;
+	amount %= 2000;
+
+	s1 = amount / 1000;
+	amount %= 1000;
+
+	print();
+
+
+
+}
